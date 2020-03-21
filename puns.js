988df7a93d27e1178a37ad010b1eefb558c9d3b2
jenbeltran <francescajenbeltran@gmail.com>
2020-03-21 16:38:57 -0400

    tea puns started

---
+++ puns.js
@@ -0,0 +1,13 @@
+//@everythingjenb
+const steepedTea = (tea) => {
+	alert('Happy Friday Everyone!');
+};
+
+//@everythingjenb
+const happyFriday = (tea) => {
+	if (tea != 'steeped') {
+		console.log("Be patient, it's Friday!");
+	} else {
+		console.log('Happy Friday, enjoy your cup!');
+	}
+};
