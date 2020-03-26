e6a5c05398103291326db4bf0c53b325a04e2542
jenbeltran <francescajenbeltran@gmail.com>
2020-03-26 15:19:08 -0400

    add html and css file

--- js-puns.js
+++ js-puns.js
@@ -1,6 +1,6 @@
 //@everythingjenb
-const steepedTea = (tea) => {
-	alert('Happy Friday Everyone!');
+const awesomeDay = (steepedTea) => {
+	alert('Have a tea-rrific day!');
 };
 
 //@everythingjenb
@@ -9,5 +9,11 @@ const happyFriday = (tea) => {
 		console.log("Be patient, it's Friday!");
 	} else {
 		console.log('Happy Friday, enjoy your cup!');
+	}
+};
+
+const getTea = (typeOfDay) => {
+	if (typeOfDay === 'awesome') {
+		console.log('Have a tea-rrific day!');
 	}
 };
